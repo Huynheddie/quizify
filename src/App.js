84ede28aed46a5eb071a060c9528ef21092f83cf
huynheddie <huynheddie@yahoo.com>
2020-06-18 16:21:29 -0700

    Added basic Spotify authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+// import Spotify from 'spotify-web-api-js';
+import SpotifyWebApi from 'spotify-web-api-js';
+import AUTHZ_CREDENTIALS from './utils/config';
+// import axios from 'axios';
+import './css/App.css';
 
 function App() {
+  const authEndpoint = 'https://accounts.spotify.com/authorize';
+  const clientId = AUTHZ_CREDENTIALS.CLIENT_ID;
+  const redirectUri = AUTHZ_CREDENTIALS.REDIRECT_URI;
+  const scopes = [
+    'user-read-private'
+  ];
+
+  const [token, setToken] = useState('');
+  const [user, setUser] = useState(null);
+
+
+  useEffect(() => {
+    let hash = window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce(function (initial, item) {
+      if (item) {
+        var parts = item.split('=');
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+      }
+      return initial;
+    }, {});
+    window.location.hash = '';
+
+    setToken(hash.access_token);
+
+  }, [])
+
+  useEffect(() => {
+    if (token) {
+      let spotifyApi = new SpotifyWebApi();
+      spotifyApi.setAccessToken(token);
+      spotifyApi.getMe().then((r) => {
+        setUser(r);
+      });
+    }
+  }, [token])
+
+  const handleLogin = () => {
+    window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello World</h1>
+     <button type="login" onClick={handleLogin}>Login</button>
+     
+     { user !== null && 
+      <div>
+        <h2>{user.display_name}</h2>
+        <h2>{user.id}</h2>
+        <img src={user.images[0].url}/>
+        <h3>Followers: {user.followers.total}</h3>
+      </div>
+     }
+     
     </div>
   );
 }
