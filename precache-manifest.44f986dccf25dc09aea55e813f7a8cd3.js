fa2b775bf7a71e7962a3b46dd8fe54a94066fb03
huynheddie <huynheddie@yahoo.com>
2020-06-30 14:00:21 -0700

    Updates

---
+++ precache-manifest.44f986dccf25dc09aea55e813f7a8cd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e2490d160f772bc13b654e3f3c97ddd",
+    "url": "/quizify/index.html"
+  },
+  {
+    "revision": "06402e3fa147493118eb",
+    "url": "/quizify/static/css/main.1cca5950.chunk.css"
+  },
+  {
+    "revision": "da081384d4d93e37eb70",
+    "url": "/quizify/static/js/2.4d2668ba.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/quizify/static/js/2.4d2668ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06402e3fa147493118eb",
+    "url": "/quizify/static/js/main.a884c700.chunk.js"
+  },
+  {
+    "revision": "e5d940cb5149c60f66cb",
+    "url": "/quizify/static/js/runtime-main.39325b68.js"
+  },
+  {
+    "revision": "daf137762beca656a3c6d7791b0d1c85",
+    "url": "/quizify/static/media/spotify.daf13776.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.024fa4fe1bc35cad76d6826dcaecd7cf.js"
+  "/quizify/precache-manifest.44f986dccf25dc09aea55e813f7a8cd3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quizify/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
